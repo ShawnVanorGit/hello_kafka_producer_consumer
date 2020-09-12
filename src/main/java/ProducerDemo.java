e389b12c9b79e142fb5a7fd7384b5e00a094ed43
shawnvan <13631230887@163.com>
2020-09-12 14:10:48 +0800

    hello_kafka_producer_consumer

---
+++ src/main/java/ProducerDemo.java
@@ -0,0 +1,34 @@
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class ProducerDemo {
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "120.27.233.226:9092");
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = null;
+        try {
+            producer = new KafkaProducer<String, String>(properties);
+            for (int i = 0; i < 100; i++) {
+                String msg = "This is Message " + i;
+                producer.send(new ProducerRecord<String, String>("test", 0,null, msg));
+                System.out.println("Sent:" + msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            producer.close();
+        }
+    }
+}
